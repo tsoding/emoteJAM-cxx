8912b03e3a4ea1d46c555b18fcdacbfc121c01fc
rexim <reximkut@gmail.com>
2021-04-26 19:22:25 +0700

    Implement ability to save the current screen to file

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "./stb_image.h"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "./stb_image_write.h"
+
 #include <SDL2/SDL.h>
 
 using namespace aids;
@@ -54,6 +57,58 @@ SDL_Texture *texture_from_file(SDL_Renderer *renderer, const char *file_path)
     return sdl_check(SDL_CreateTextureFromSurface(renderer, surface));
 }
 
+void render_state(SDL_Renderer *renderer,
+                  SDL_Texture *texture,
+                  const SDL_Rect *texture_rect)
+{
+    sdl_check(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
+    sdl_check(SDL_RenderClear(renderer));
+
+    sdl_check(SDL_RenderCopy(renderer,
+                             texture,
+                             texture_rect,
+                             texture_rect));
+
+    SDL_RenderPresent(renderer);
+}
+
+void save_renderer_to_png_file(SDL_Renderer *renderer,
+                               const char *file_path)
+{
+    SDL_Rect target_rect = {};
+    Uint32 target_format = SDL_PIXELFORMAT_ABGR8888;
+    sdl_check(SDL_GetRendererOutputSize(
+                  renderer,
+                  &target_rect.w,
+                  &target_rect.h));
+
+    Uint32 *pixels = static_cast<Uint32*>(malloc(sizeof(Uint32) * target_rect.w * target_rect.h));
+    if (pixels == nullptr) {
+        panic("ERROR: save_renderer_to_png_file: could not allocate memory for pixels: ", strerror(errno));
+    }
+    defer(free(pixels));
+
+
+    {
+        Uint32 begin = SDL_GetTicks();
+        sdl_check(SDL_RenderReadPixels(
+                      renderer,
+                      &target_rect,
+                      target_format,
+                      pixels,
+                      sizeof(*pixels) * target_rect.w));
+        println(stdout, "read pixels: ", SDL_GetTicks() - begin);
+    }
+
+    {
+        Uint32 begin = SDL_GetTicks();
+        if (!stbi_write_png(file_path, target_rect.w, target_rect.h, 4, pixels, sizeof(*pixels) * target_rect.w)) {
+            panic("ERROR: could not save image to file ", file_path, ": ", strerror(errno));
+        }
+        println(stdout, "stbi_write_png: ", SDL_GetTicks() - begin);
+    }
+}
+
 int main(int argc, char **argv)
 {
     Args args = {argc, argv};
@@ -69,11 +124,13 @@ int main(int argc, char **argv)
     sdl_check(SDL_Init(SDL_INIT_VIDEO));
     defer(SDL_Quit());
 
+    const size_t DISPLAY_WIDTH = 800;
+    const size_t DISPLAY_HEIGHT = 800;
     SDL_Window *window =
         sdl_check(SDL_CreateWindow(
                       "emoteJAM",
                       0, 0,
-                      800, 600,
+                      DISPLAY_WIDTH, DISPLAY_HEIGHT,
                       SDL_WINDOW_RESIZABLE));
     defer(SDL_DestroyWindow(window));
 
@@ -81,14 +138,36 @@ int main(int argc, char **argv)
         sdl_check(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
     defer(SDL_DestroyRenderer(renderer));
 
-    SDL_Texture *texture = texture_from_file(renderer, emote_image_path);
-    defer(SDL_DestroyTexture(texture));
+    {
+        SDL_RendererInfo info = {};
+        sdl_check(SDL_GetRendererInfo(renderer, &info));
+        if (!(info.flags & SDL_RENDERER_TARGETTEXTURE)) {
+            panic("ERROR: SDL Renderer Target Texture is NOT supported!");
+        } else {
+            println(stderr, "INFO: SDL Renderer Target Texture is supported!");
+        }
+    }
 
-    SDL_Rect texture_rect = {};
+    SDL_Texture *emote_texture = texture_from_file(renderer, emote_image_path);
+    defer(SDL_DestroyTexture(emote_texture));
+
+    SDL_Rect emote_texture_rect = {};
+    Uint32 emote_texture_format = 0;
     sdl_check(SDL_QueryTexture(
-                  texture,
-                  NULL, NULL,
-                  &texture_rect.w, &texture_rect.h));
+                  emote_texture,
+                  &emote_texture_format,
+                  NULL,
+                  &emote_texture_rect.w,
+                  &emote_texture_rect.h));
+
+    SDL_Texture *target = sdl_check(
+                              SDL_CreateTexture(
+                                  renderer,
+                                  SDL_PIXELFORMAT_ABGR8888,
+                                  SDL_TEXTUREACCESS_TARGET,
+                                  emote_texture_rect.w,
+                                  emote_texture_rect.h));
+
 
     bool quit = false;
     while (!quit) {
@@ -99,18 +178,26 @@ int main(int argc, char **argv)
                 quit = true;
             }
             break;
+
+            case SDL_KEYDOWN: {
+                switch (event.key.keysym.sym) {
+                case SDLK_s: {
+                    sdl_check(SDL_SetRenderTarget(renderer, target));
+                    defer(SDL_SetRenderTarget(renderer, NULL));
+
+                    render_state(renderer, emote_texture, &emote_texture_rect);
+                    Uint32 begin = SDL_GetTicks();
+                    save_renderer_to_png_file(renderer, "output.png");
+                    println(stdout, SDL_GetTicks() - begin);
+                }
+                break;
+                }
+            }
+            break;
             }
         }
 
-        sdl_check(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
-        sdl_check(SDL_RenderClear(renderer));
-
-        sdl_check(SDL_RenderCopy(renderer,
-                                 texture,
-                                 &texture_rect,
-                                 &texture_rect));
-
-        SDL_RenderPresent(renderer);
+        render_state(renderer, emote_texture, &emote_texture_rect);
     }
 
     return 0;
